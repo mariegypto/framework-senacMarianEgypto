00eafbdeea31c544681d4cee544ae1d3af279388
MARIANA EGYPTO FEIGEL <mariana.0769@aluno.pr.senac.br>
2022-08-11 22:23:19 -0300

    atualizando index e bootstrap

--- bootstrap/Env.php
+++ bootstrap/Env.php
@@ -1,8 +1,24 @@
-<?php
+<?php 
 
-class Env {
-    
-    public function __construct(){ // construct método invocado ao instanciar uma classe
+namespace Bootstrap;
 
+class Env{
+
+    public static function execute(){
+        $contentOfEnvFile = file_get_contents(__DIR__ . "\..\.env");
+        $arrayEnv = explode("\n",$contentOfEnvFile);
+
+        foreach($arrayEnv as $value){
+            $keyAndValue = explode ("=", $value);
+
+            if(!isset($keyAndValue[1])){//isset verifica se existe ! nega
+                continue;
+            }
+            $nameOfVariable = $keyAndValue[0];
+            $valueOfVariable = $keyAndValue[1];
+
+            $_ENV[$nameOfVariable] = $valueOfVariable;
+
+        }
     }
 }
--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php
 
-function dd($input){ //mostra a variavel e mata o código ao invocar ela, da um breakpoint, usa para ir testando a variável
-    var_dump($input); //mostra todas as informações do objeto
-    die();//break
-}
-
 $mainPosition = __DIR__;
 
-require_once("{$mainPosition}\bootstrap\Env.php");//require_once invoca no script. Forma de concatenar de forma inteligente variáveis com strings
+//require_once("{$mainPosition}\bootstrap\Env.php");//require_once invoca no script. Forma de concatenar de forma inteligente variáveis com strings
 
-$env = new Env();
-dd($env);//dump
+//$env = new Env();
+//dd($env);//dump
 
 //dd(__DIR__);//constante mágica->tem a posição absoluta do diretório, que é o que faz referência nos arquivos
+
+//dd(__DIR__); //metofd magico a posicao do diretorio,
+
+
+require_once("{$mainPosition}\helper\helper.php");
+require_once("{$mainPosition}\\vendor\autoload.php");
+
+use Bootstrap\Env;
+use App\FrameworkTools\ProcessServerElements;
+
+Env::execute();
+
+$processServerElements = ProcessServerElements::start();
+dd($processServerElements);
